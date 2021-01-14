7f3056b2f7beaecbd180d0735a526dd4a2ad247f
Dylan <dylancooper44@gmail.com>
2021-01-13 21:25:36 -0500

    second commit

---
+++ GasMileageCalculator/Controllers/GasMileageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GasMileageCalculator.Models.GasMileage;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GasMileageCalculator.Controllers
+{
+    public class GasMileageController : Controller
+    {
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult FormResult(FormResultViewModel model)
+        {
+            // do some math and shit
+            model.FuelRange = model.MPG * model.Gallons;
+
+            if (model.TripDistance <= model.FuelRange)
+            {
+                model.SuccessfulTrip = "Yes";
+            }
+            else
+            {
+                model.SuccessfulTrip = "No Way Jose";
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ GasMileageCalculator/Models/GasMileage/FormResultViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GasMileageCalculator.Models.GasMileage
+{
+    public class FormResultViewModel
+    {
+        public int MPG { get; set; }
+
+        public int Gallons { get; set; }
+
+        public int FuelRange { get; set; }
+
+        public int TripDistance { get; set; }
+
+        public string SuccessfulTrip { get; set; }
+
+
+    }
+}
